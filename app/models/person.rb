c6313fceb4d0292a8a7b0e24a341383c41e5e923
Jeppe Liisberg <jeppe@liisberg.net>
2009-03-01 21:23:51 +0100

    friendly url bugfix

--- app/models/person.rb
+++ app/models/person.rb
@@ -41,7 +41,7 @@ class Person < ActiveRecord::Base
   end
   
   def to_param
-    id.to_s+"-"+name.gsub(/[Ææ]/,'ae').gsub(/[Øø]/,'o').downcase.gsub(/[^a-z0-9\s_-]+/,'').gsub(/[\s_-]+/,'-'))
+    id.to_s+"-"+(name.gsub(/[Ææ]/,'ae').gsub(/[Øø]/,'o').downcase.gsub(/[^a-z0-9\s_-]+/,'').gsub(/[\s_-]+/,'-'))
   end
 
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -7,7 +7,7 @@ class Place < ActiveRecord::Base
   validates_format_of :web_address, :with => /^(http|https):\/\/?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix, :message => "er ugyldig (skal indeholde 'http').", :allow_blank => true
   
   def to_param
-    id.to_s+"-"+name.gsub(/[Ææ]/,'ae').gsub(/[Øø]/,'o').downcase.gsub(/[^a-z0-9\s_-]+/,'').gsub(/[\s_-]+/,'-'))
+    id.to_s+"-"+(name.gsub(/[Ææ]/,'ae').gsub(/[Øø]/,'o').downcase.gsub(/[^a-z0-9\s_-]+/,'').gsub(/[\s_-]+/,'-'))
   end
 
 end
